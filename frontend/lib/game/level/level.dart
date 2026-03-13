b437e658f4a992a1ccf9d378179de7b268cf4c58
Ractopen <ractenopen@gmail.com>
2026-03-13 17:31:13 +0800

    SCRUM-33: Clean up tiles and assets for flame_tiled compatibility

--- frontend/lib/game/level/level.dart
+++ frontend/lib/game/level/level.dart
@@ -4,7 +4,15 @@ import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/foundation.dart';
 import '../actors/player.dart';
 import 'collision_block.dart';
+import '../objects/door.dart';
+import '../objects/bed.dart';
 
+/// Tiled Object Classes/Names handled:
+/// - Spawn: Player starting position
+/// - Door: Interactable door with collision
+/// - Bed: Passable bed object
+/// - Turret: Placeholder for future turret
+/// - EnergyMaker: Placeholder for future energy maker
 class Level extends World {
   final String levelName;
   final Player player;
@@ -19,6 +27,12 @@ class Level extends World {
       level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
       add(level);
 
+      // Hide the Object tile layer as we spawn components for them
+      final objectTileLayer = level.tileMap.getLayer<TileLayer>('Object');
+      if (objectTileLayer != null) {
+        objectTileLayer.visible = false;
+      }
+
       final spawnPointLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
       if (spawnPointLayer != null) {
         for (final spawnPoint in spawnPointLayer.objects) {
@@ -30,8 +44,42 @@ class Level extends World {
           }
         }
       }
+
+      final objectLayer = level.tileMap.getLayer<ObjectGroup>('ObjectLayer');
+      if (objectLayer != null) {
+        for (final object in objectLayer.objects) {
+          final String type = object.class_.isEmpty || object.class_ == 'Object' ? object.name : object.class_;
+          
+          switch (type) {
+            case 'Spawn':
+              player.position = Vector2(object.x + object.width / 2, object.y + object.height);
+              break;
+            case 'Door':
+              final door = Door(
+                position: Vector2(object.x, object.y),
+                size: Vector2(object.width, object.height),
+              );
+              add(door);
+              collisions.add(door.collisionBlock);
+              break;
+            case 'Bed':
+              final bed = Bed(
+                position: Vector2(object.x, object.y),
+                size: Vector2(object.width, object.height),
+              );
+              add(bed);
+              // Bed is passable as requested, so we don't add it to collisions
+              break;
+            case 'Turret':
+              // Turret(position: Vector2(object.x, object.y), size: Vector2(object.width, object.height));
+              break;
+            case 'EnergyMaker':
+              // EnergyMaker(position: Vector2(object.x, object.y), size: Vector2(object.width, object.height));
+              break;
+          }
+        }
+      }
       
-      // Ensure player is added once after trying to find spawnpoint
       if (!player.isMounted) {
         add(player);
       }
---
+++ frontend/lib/game/objects/door.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:dreamhunter/game/dreamhunter_game.dart';
+import '../level/collision_block.dart';
+
+class Door extends SpriteComponent with HasGameReference<DreamHunterGame>, TapCallbacks {
+  bool isOpen;
+  late final Sprite _closedSprite;
+  late final Sprite _openSprite;
+  final CollisionBlock collisionBlock;
+
+  Door({
+    required Vector2 position,
+    required Vector2 size,
+    this.isOpen = false,
+  })  : collisionBlock = CollisionBlock(
+          position: position.clone(),
+          size: size.clone(),
+          isPassable: isOpen,
+        ),
+        super(position: position, size: size);
+
+  @override
+  FutureOr<void> onLoad() async {
+    priority = 1;
+    
+    _closedSprite = await game.loadSprite(
+      'door(32x32).png',
+      srcPosition: Vector2(0, 0),
+      srcSize: Vector2(32, 32),
+    );
+    
+    _openSprite = await game.loadSprite(
+      'door(32x32).png',
+      srcPosition: Vector2(32, 0),
+      srcSize: Vector2(32, 32),
+    );
+
+    sprite = isOpen ? _openSprite : _closedSprite;
+    
+    return super.onLoad();
+  }
+
+  void toggleDoor() {
+    isOpen = !isOpen;
+    sprite = isOpen ? _openSprite : _closedSprite;
+    collisionBlock.isPassable = isOpen;
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    toggleDoor();
+  }
+}
